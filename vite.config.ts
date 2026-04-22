8cfeda2b3988d9d7e97680b52f247df2e9dd2aed
MaffeAoki <aokimaffe@gmail.com>
2026-04-22 20:51:24 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,17 @@
-{
-  "compilerOptions": {
-    "target": "ES2022",
-    "experimentalDecorators": true,
-    "useDefineForClassFields": false,
-    "module": "ESNext",
-    "lib": [
-      "ES2022",
-      "DOM",
-      "DOM.Iterable"
-    ],
-    "skipLibCheck": true,
-    "moduleResolution": "bundler",
-    "isolatedModules": true,
-    "moduleDetection": "force",
-    "allowJs": true,
-    "jsx": "react-jsx",
-    "paths": {
-      "@/*": [
-        "./*"
-      ]
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig, loadEnv} from 'vite';
+
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, '.', '');
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
-    "allowImportingTsExtensions": true,
-    "noEmit": true
-  }
-}
+    server: {
+      hmr: process.env.DISABLE_HMR !== 'true',
+    },
+  };
+});
